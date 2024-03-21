11bc93f25096cf78f9c6c04d6c2698719abe97c1
FANGFANG1757 <grace.gao13@gmail.com>
2024-03-21 17:55:00 +0800

    update after review 2

--- src/structures/queue.js
+++ src/structures/queue.js
@@ -1,21 +1,11 @@
-class MyQueue{
-    elements = [];
-    
-    push(element){ 
-        this.elements.push(element);
+export class Queue {
+  elements = [];
 
-    }
-    
-    shift(){
-        return this.elements.shift();
-    }
+  push(element) {
+    this.elements.push(element);
+  }
+
+  shift() {
+    return this.elements.shift();
+  }
 }
-
-const QUEUE = new MyQueue();
-QUEUE.push(1);
-QUEUE.push(2);
-QUEUE.push(3);
-QUEUE.shift();
-
-
-export default MyQueue;
