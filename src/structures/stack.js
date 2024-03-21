a5514688c60e1d8bb7adf3ece590db37629f8a98
FANGFANG1757 <grace.gao13@gmail.com>
2024-03-21 18:18:10 +0800

    refactor stack class

--- src/structures/stack.js
+++ src/structures/stack.js
@@ -1,22 +1,11 @@
-class myStack {
-  constructor() {
-    this.elements = [];
-  }
+export class Stack {
+  elements = [];
+
   push(element) {
     this.elements.push(element);
   }
+
   pop() {
     return this.elements.pop();
   }
 }
-const stack = new myStack();
-
-stack.push(1);
-stack.push(2);
-stack.push(3);
-console.log(stack);
-stack.pop();
-console.log(stack);
-
-
-
