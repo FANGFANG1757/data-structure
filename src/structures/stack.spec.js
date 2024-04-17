5a87ef0cee56045c9badde5978785a3871a33fa6
FANGFANG1757 <grace.gao13@gmail.com>
2024-04-17 21:43:30 +0800

    test: add test for Stack

---
+++ src/structures/stack.spec.js
@@ -0,0 +1,27 @@
+import { Stack } from "./stack";
+
+describe("Stack", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  test("push method adds an element to the stack", () => {
+    stack.push(1);
+    expect(stack.elements).toEqual([1]);
+  });
+
+  test("pop method removes and returns the last element from the stack", () => {
+    stack.push(1);
+    stack.push(2);
+    const poppedElement = stack.pop();
+    expect(poppedElement).toBe(2);
+    expect(stack.elements).toEqual([1]);
+  });
+  
+  test('pop returns undefined if stack is empty',()=>{
+    const poppedElement = stack.pop();
+    expect(poppedElement).toBeUndefined();
+  })
+});
